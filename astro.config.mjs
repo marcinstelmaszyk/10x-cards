02e8a7ca20799940c6480db0e21491fee560e386
Marcin Stelmaszyk <marcin.stelmaszyk@huuugegames.com>
2025-04-18 20:12:20 +0200

    feat: inject MessageChannel polyfill via Vite banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,26 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
+// Polyfill code to be injected
+const messageChannelPolyfill = `
+if (typeof MessageChannel === 'undefined') {
+  class MessagePortPolyfill {
+    postMessage() { /* No-op */ }
+    addEventListener() { /* No-op */ }
+    removeEventListener() { /* No-op */ }
+    dispatchEvent() { return true; }
+  }
+  class MessageChannelPolyfill {
+    constructor() {
+      this.port1 = new MessagePortPolyfill();
+      this.port2 = new MessagePortPolyfill();
+    }
+  }
+  globalThis.MessageChannel = MessageChannelPolyfill;
+  globalThis.MessagePort = MessagePortPolyfill;
+}
+`;
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
@@ -27,6 +47,14 @@ export default defineConfig({
     plugins: [tailwindcss()],
     ssr: {
       noExternal: ["react-dom/server"],
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          // Inject the polyfill at the very beginning of the worker bundle
+          banner: messageChannelPolyfill,
+        },
+      },
     },
   },
 });
